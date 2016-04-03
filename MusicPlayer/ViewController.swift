be2b94e85bf8375e55389bc4924569f5d1aabe9d
廖彬彬 <ufolbb@liaobinbindemac.local>
2016-04-03 02:28:29 -0700

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by 廖彬彬 on 16/4/3.
+//  Copyright © 2016年 廖彬彬. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
